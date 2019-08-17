c4457b0f5bf187096a192c8d92a6348ae4e0929c
Ashwin R C <ashwin2001achu@gmail.com>
2019-08-17 03:25:40 +0000

    onclite: properly inherit sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -137,7 +137,7 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 TARGET_RIL_VARIANT := caf
 
 # Sepolicy
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/qcom/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += device/qcom/sepolicy/public
 
